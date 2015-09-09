348edd9c45af2add64c9571e71ab6ab3774e359f
ynunokawa <yusuke_nunokawa@esrij.com>
2015-09-09 14:50:10 +0900

    StatusMonitoringウィジェットの追加

---
+++ widgets/StatusMonitoring/setting/nls/ja/strings.js
@@ -0,0 +1,7 @@
+﻿define({
+		targetLayer: "レイヤーの選択",
+		targetField: "対象属性フィールドの選択",
+		targetIdentifiedField: "識別用フィールドの選択",
+		viewMessage: "メッセージ ダイアログの表示",
+		messageNoMap: "フィーチャ レイヤーがありません。このウィジェットを使うには、フィーチャ レイヤーを含む Web マップを選択してください。"
+});
---
+++ widgets/StatusMonitoring/setting/nls/strings.js
@@ -0,0 +1,10 @@
+define({
+	root: {
+		targetLayer: "Choose a layer",
+		targetField: "Choose a target field",
+		targetIdentifiedField: "Choose a identified field",
+		viewMessage: "View a message dialog",
+		messageNoMap: "Your map has no feature layer. Select a map to contain a feature layer."
+	},
+  "ja": true
+});
