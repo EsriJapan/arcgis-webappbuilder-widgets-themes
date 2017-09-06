bfb1217f4404429d7604c4960527577e9e5da0f3
Ayako Suzuki <ayako_suzuki@esrij.com>
2017-09-06 11:10:09 +0900

    issue #5

--- widgets/Clustering/Widget.js
+++ widgets/Clustering/Widget.js
@@ -17,55 +17,20 @@
 define([
   'dojo/_base/declare',
   'jimu/BaseWidget',
-
-  "esri/arcgis/utils",
-  "dojo/_base/array",
-  "dojo/_base/lang",
-  "dojo/on",
-  "dojo/dom-style",
-  "dojo/dom",
-
-  "esri/symbols/SimpleMarkerSymbol",
-  "esri/symbols/SimpleLineSymbol",
-  "esri/renderers/ClassBreaksRenderer",
-  "esri/Color",
-
-  "./clusterfeaturelayer",
-
-  "dijit/form/Select",
-], function(
-  declare,
-  BaseWidget,
-  //_WidgetsInTemplateMixin,
-
-  arcgisUtils,
-  array,
-  lang,
-  on,
-  domStyle,
-  dom,
-
-  SimpleMarkerSymbol,
-  SimpleLineSymbol,
-  ClassBreaksRenderer,
-  Color,
-
-  ClusterLayer,
-
-  Select
-) {
-  var clazz = declare([BaseWidget], {
+  'dojo/_base/array',
+  'esri/symbols/SimpleMarkerSymbol',
+  'esri/symbols/SimpleLineSymbol',
+  'esri/renderers/ClassBreaksRenderer',
+  'esri/Color',
+  './clusterfeaturelayer'
+], function(declare, BaseWidget, array, SimpleMarkerSymbol, SimpleLineSymbol, ClassBreaksRenderer, Color, ClusterLayer) {
+  return declare([BaseWidget], {
     name: 'Clustering',
     baseClass: 'jimu-widget-clustering',
+    clusterLayer: null,
 
     postCreate: function() {
-      //this.inherited(arguments);
-      console.log('postCreate');
-    },
-
-    startup: function() {
       this.inherited(arguments);
-      console.log('startup');
 
       // 選択レイヤーID
       var id;
@@ -73,40 +38,32 @@ define([
       var map = this.map;
       // 設定情報
       var json = this.config.clustering;
-      console.log(json);
       // 設定１:選択レイヤー（タイトル）
       var title = json.selectLayer;
-      console.log("selected layer title: ", title);
 
       // マップの操作レイヤー
       var layers = map.itemInfo.itemData.operationalLayers;
       array.some(layers, function(layer) {
-        if(layer.title === title){
+        if (layer.title === title) {
           id = layer.id;
           return true;
         } else {
           return false;
         }
       });
-      console.log("selected layer id: ", id);
-
-      var self = this;
-      // クラスタレイヤー
-      var clusterLayer;
 
       // 選択レイヤーがマップ上に存在する場合
-      if(id){
+      if (id) {
         // 選択レイヤーIDからレイヤーを取得
         var selectedLayer = map.getLayer(id);
         this.selectedLayer = selectedLayer;
         // クラスターレイヤーの作成
-        if(!clusterLayer) {
-          console.log("creating clusterlayer!");
-          clusterLayer = new ClusterLayer({
-             title: "クラスター表示: " + title,
+        if (!this.clusterLayer) {
+          this.clusterLayer = new ClusterLayer({
+             title: 'クラスター表示: ' + title,
              url: map.getLayer(id).url,
              distance: json.clusterSize, // 設定２:クラスター集計範囲
-             id: "cluster",
+             id: 'cluster',
              labelColor: json.labelColor, // 設定４:ラベル色
              resolution: map.extent.getWidth() / map.width,
              useDefaultSymbol: true,
@@ -116,11 +73,11 @@ define([
           });
 
           // 設定３:シンボル色
-          var lineColor = new Color([json.symbolColor.r,json.symbolColor.g,json.symbolColor.b,0.35]);
-          var markerColor = new Color([json.symbolColor.r,json.symbolColor.g,json.symbolColor.b,0.75]);
-          var symbol = new SimpleMarkerSymbol("circle", 25,
-            new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID, lineColor, 15),
-            markerColor);
+          var lineColor = new Color([json.symbolColor.r, json.symbolColor.g, json.symbolColor.b, 0.35]);
+          var markerColor = new Color([json.symbolColor.r, json.symbolColor.g, json.symbolColor.b, 0.75]);
+          var symbol = new SimpleMarkerSymbol('circle', 25,
+                       new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID, lineColor, 15),
+                       markerColor);
 
           // レンダラー（ポイント数に応じて数値分類）
           var renderer = new ClassBreaksRenderer(symbol, 'clusterCount');
@@ -135,44 +92,26 @@ define([
                       new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID, lineColor, 15),
                       markerColor);
           var xlarge = new SimpleMarkerSymbol('circle', 110,
-                      new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID, lineColor, 15),
-                      markerColor);
+                       new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID, lineColor, 15),
+                       markerColor);
 
           renderer.addBreak(2, 10, small);
           renderer.addBreak(10, 25, medium);
           renderer.addBreak(25, 100, large);
           renderer.addBreak(100, Infinity, xlarge);
 
-          clusterLayer.setRenderer(renderer);
-
-          map.addLayer(clusterLayer);
-          this.clusterLayer = clusterLayer;
-
-
-
-          console.log("map: ", map);
-          console.log("this.map: ", this.map);
-          //this.map = map;
-          //console.log(map);
-          //map.itemInfo.itemData.operationalLayers.push(clusterLayer);
+          this.clusterLayer.setRenderer(renderer);
         }
-        else {
-          //this.clusterLayer.show();
-        }
-
       }
     },
 
-    onOpen: function(evt){
-      console.log('onOpen');
+    onOpen: function() {
+      this.map.addLayer(this.clusterLayer);
+      //this.map.itemInfo.itemData.operationalLayers.push(clusterLayer);
     },
 
-    onClose: function(evt){
-      console.log('onClose');
-      // マップからクラスターレイヤーを除外
-      this.map.removeLayer(this.map.getLayer("cluster"));
-      //this.map.itemData.oprationalLayers.push(clusterLayer);
+    onClose: function() {
+      this.map.removeLayer(this.map.getLayer('cluster'));
     }
   });
-  return clazz;
 });
